da1cf71813b9fbe48efb8a91e9501895115a0271
Wojciech Blachowski <wblachowsky@gmail.com>
2022-03-11 09:39:59 +0100

    Refactoring

--- src/main/java/com/wblachowski/wordnet/example/Hyponyms.java
+++ src/main/java/com/wblachowski/wordnet/example/Hyponyms.java
@@ -5,8 +5,6 @@ import net.sf.extjwnl.JWNLException;
 import net.sf.extjwnl.data.POS;
 import net.sf.extjwnl.data.PointerUtils;
 import net.sf.extjwnl.data.Word;
-import net.sf.extjwnl.data.list.PointerTargetNode;
-import net.sf.extjwnl.data.list.PointerTargetNodeList;
 import net.sf.extjwnl.dictionary.Dictionary;
 
 public class Hyponyms {
@@ -14,10 +12,10 @@ public class Hyponyms {
         var dict = Dictionary.getDefaultResourceInstance();
         var indexWord = dict.lookupIndexWord(POS.NOUN, "dog");
         var mainSense = indexWord.getSenses().get(0);
-        PointerTargetNodeList hyponyms = PointerUtils.getDirectHyponyms(mainSense);
+        var hyponyms = PointerUtils.getDirectHyponyms(mainSense);
         System.out.println(String.format("Definition of dog:%nt%s%n%nDirect hyponyms:", mainSense.getGloss()));
 
-        for (PointerTargetNode node : hyponyms) {
+        for (var node : hyponyms) {
             var synset = node.getSynset();
             var lemmas = synset.getWords().stream().map(Word::getLemma).collect(Collectors.joining(", "));
             var definition = synset.getGloss();
